458914b587645ae99a621ab1cea88bc386549427
Shon Feder <shon.feder@gmail.com>
2021-03-05 14:54:07 -0500

    Preserve (most) content of logical units

--- src/pandoc.rs
+++ src/pandoc.rs
@@ -3,9 +3,12 @@
 //!
 
 use pandoc_ast::{Block, Inline, Pandoc, QuoteType};
-use std::io::{Read, Write};
 use std::path::Path;
 use std::process::{Command, Stdio};
+use std::{
+    convert::TryInto,
+    io::{Read, Write},
+};
 
 static PANDOC: &str = "pandoc";
 static ARGS: &[&str] = &["--standalone", "--from", "markdown-smart", "--to", "json"];
@@ -53,6 +56,7 @@ pub fn parse_string(s: &str) -> Result {
     pandoc_from_bytes(&bytes)
 }
 
+// TODO Use this to support more integrated recovery of nested markdown
 /// Renders a pandoc AST into a markdown string
 // pub fn render_ast(p: &Pandoc) -> std::result::Result<String, String> {
 //     let process = Command::new(PANDOC)
@@ -104,8 +108,8 @@ pub fn parse_file(path: &Path) -> Result {
 }
 
 /// # Parsing the pandoc AST
-///
-pub fn inlines_to_string(inlines: &[pandoc_ast::Inline]) -> String {
+#[allow(clippy::ptr_arg)]
+pub fn inlines_to_string(inlines: &Vec<Inline>) -> String {
     inlines
         .iter()
         .map(inline_to_string)
@@ -120,7 +124,7 @@ pub fn inline_to_string(i: &pandoc_ast::Inline) -> String {
         Inline::Strong(v) => format!("**{}**", inlines_to_string(v),),
         Inline::Space => " ".to_string(),
         Inline::SoftBreak => "\n".to_string(),
-        Inline::LineBreak => "\\\n".to_string(),
+        Inline::LineBreak => "\\n".to_string(),
         Inline::Quoted(t, v) => match t {
             QuoteType::SingleQuote => format!("'{}'", inlines_to_string(v)),
             QuoteType::DoubleQuote => format!("\"{}\"", inlines_to_string(v)),
@@ -128,7 +132,16 @@ pub fn inline_to_string(i: &pandoc_ast::Inline) -> String {
         Inline::Link(_, v, (url, _)) => format!("[{}]({})", inlines_to_string(v), url,),
         Inline::Image(_, v, (url, _)) => format!("![{}]({})", inlines_to_string(v), url,),
         Inline::Code(_, s) => format!("`{}`", s),
-        _ => "TODO".to_string(),
+        Inline::Underline(il) => inlines_to_string(il),
+        Inline::Strikeout(il) => format!("~~{}~~", inlines_to_string(il)),
+        Inline::Superscript(il) => format!("^{}^", inlines_to_string(il)),
+        Inline::Subscript(il) => format!("~{}~", inlines_to_string(il)),
+        Inline::SmallCaps(il) => format!("[{}]{{.smallcaps}}", inlines_to_string(il)),
+        Inline::Cite(_, il) => inlines_to_string(il),
+        Inline::Math(_, s) => s.clone(),
+        Inline::RawInline(_, s) => s.clone(),
+        Inline::Note(bl) => blocks_to_string(bl),
+        Inline::Span(_, il) => inlines_to_string(il),
     }
 }
 
@@ -149,10 +162,59 @@ pub fn blocks_to_string(blocks: &Vec<Block>) -> String {
         .join("\n\n")
 }
 
+#[allow(clippy::ptr_arg)]
+fn line_blocks_to_string(lines: &Vec<Vec<Inline>>) -> String {
+    lines
+        .iter()
+        .map(inlines_to_string)
+        .collect::<Vec<String>>()
+        .join("\n\n")
+}
+
+#[allow(clippy::ptr_arg)]
+fn block_quote_to_string(blocks: &Vec<Block>) -> String {
+    blocks
+        .iter()
+        .map(block_to_string)
+        .map(|b| format!("> {}", b))
+        .collect::<Vec<String>>()
+        .join("\n")
+}
+
+#[allow(clippy::ptr_arg)]
+fn list_to_string(prefix: &str, blocks: &Vec<Vec<Block>>) -> String {
+    blocks
+        .iter()
+        .map(blocks_to_string)
+        .map(|b| format!("{} {}", prefix, b))
+        .collect::<Vec<String>>()
+        .join("\n")
+}
+
+#[allow(clippy::ptr_arg)]
+fn header_to_string(level: &i64, content: &Vec<Inline>) -> String {
+    // Pandoc would violate its own spec if it allowed more than 6 heading levels
+    // so we should be safe to unwrap here.
+    let hashes = "#".repeat((*level).try_into().unwrap());
+    format!("{} {}", hashes, inlines_to_string(content))
+}
+
 pub fn block_to_string(b: &Block) -> String {
     match b {
         Block::Plain(v) => inlines_to_string(v),
         Block::Para(v) => inlines_to_string(v),
-        _ => "TODO".to_string(),
+        Block::LineBlock(v) => line_blocks_to_string(v),
+        Block::CodeBlock(_, s) => format!("```\n{}\n```", s),
+        Block::RawBlock(_, s) => s.into(),
+        Block::BlockQuote(v) => block_quote_to_string(v),
+        Block::OrderedList(_, v) => list_to_string("1.", v),
+        Block::BulletList(v) => list_to_string("-", v),
+        Block::Header(level, _, v) => header_to_string(level, v),
+        Block::HorizontalRule => "---".into(),
+        Block::Null => "".into(),
+        // Currently unspported values
+        Block::Div(_, _) => "<!-- WARNING: nested div omitted -->".into(),
+        Block::DefinitionList(_) => "<!-- WARNING: nested definition lists omitted -->".into(),
+        Block::Table(_, _, _, _, _, _) => "<!-- WARNING: nested table omitted -->".into(),
     }
 }
---
+++ tests/parsing-spec.md
@@ -0,0 +1,34 @@
+# Parsing logical units
+
+|PARSE-SPECS.1|
+: We can parse a file of logical units into different formats, preserving all
+  critical content of the logical unit content.
+
+## Serialization Format
+
+Supported formats include:
+
+|PARSE-SPECS.1::JSON.1|
+: Must support parsing a file of specs into JSON.
+
+|PARSE-SPECS.1::CSV.1|
+: Must support parse a file of specs into CSV.
+
+## Content
+
+|PARSE-SPECS.1::CONTENT.1::MULTI-PARA.1|
+: The content of logical units must be preserved.
+: Even when it spans multiple paragraphs.
+: - Or
+ - includes
+ - lists
+
+|PARSE-SPECS.1::INLINE.1|
+: The folowing inline styling must be preserved:
+: - **Strong** (__both__ ways)
+ - *Emphasizes* (_both_ ways)
+ - ~~Strikethrough~~
+ - `code`
+ - [links](/url)
+ - ![images](/url)
+ - [smallcaps]{.smallcaps}
