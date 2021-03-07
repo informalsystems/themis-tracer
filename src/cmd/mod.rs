843e484cb6c072e5bbe1c937056497b0e4ae7d11
Shon Feder <shon.feder@gmail.com>
2021-03-07 18:51:17 -0500

    Move the cmd module docs into the mod.rs file

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,8 +1,21 @@
-mod context;
+//! The `cmd` submodule contains all the logic for CLI.
+
+/// ## Special submodules
+
+/// Definition of the CLI
+pub mod opt;
+
+/// Utility for serialized output formats
 mod format;
+
+/// ## Subcommand executors
+///
+/// Each of the following modules contains an executors for the corresponding
+/// subcommand defined in `opt`. Each module exports a `run` function for this
+/// purpose.
+mod context;
 mod init;
 mod parse;
 mod repo;
+mod sync;
 mod unit;
-
-pub mod opt;
