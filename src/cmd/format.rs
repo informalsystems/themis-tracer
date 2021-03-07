097c27eb8ec9761c39adfc99bff9eb0f3a778c01
Shon Feder <shon.feder@gmail.com>
2021-03-07 13:24:51 -0500

    Refactor format output into format module

--- src/cmd/format.rs
+++ src/cmd/format.rs
@@ -1,4 +1,8 @@
-use std::fmt;
+use {
+    crate::logical_unit::LogicalUnit,
+    anyhow::Result,
+    std::{fmt, io},
+};
 
 impl fmt::Display for ParseFormatError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -15,6 +19,39 @@ pub struct ParseFormatError(String);
 pub enum Format {
     Csv,
     Json,
+}
+
+impl Format {
+    pub fn units(&self, mut lus: Vec<LogicalUnit>) -> Result<()> {
+        lus.sort();
+
+        match self {
+            Format::Csv => {
+                // See https://docs.rs/csv/1.1.3/csv/tutorial/index.html#writing-csv
+                let mut wtr = csv::WriterBuilder::new()
+                    .has_headers(false)
+                    .from_writer(io::stdout());
+                // TODO include headers?
+                // Write the headers
+                // wtr.serialize([
+                //     "tag",
+                //     "kind",
+                //     "repo",
+                //     "file",
+                //     "line",
+                //     "content",
+                //     "references",
+                // ])?;
+                lus.iter()
+                    .try_for_each(|x| wtr.serialize(x).map_err(|e| e.into()))
+            }
+            Format::Json => lus.iter().try_for_each(|x| {
+                serde_json::to_string(x)
+                    .map_err(|e| e.into())
+                    .map(|x| println!("{}", x))
+            }),
+        }
+    }
 }
 
 impl fmt::Display for Format {
