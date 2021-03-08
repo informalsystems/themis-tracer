5c5b511f82456eef6bcd7c204fc373dfcef24cef
Shon Feder <shon.feder@gmail.com>
2021-03-07 19:24:41 -0500

    Implement sync command
    Closes #17
    
    Adds a `sync` command that will update all the logical units from all
    the repositories associated with the current working context.

--- src/cmd/sync.rs
+++ src/cmd/sync.rs
@@ -1,1 +1,14 @@
+use {
+    crate::{cmd, db},
+    anyhow::Result,
+};
 
+pub fn run() -> Result<()> {
+    let conn = db::connection()?;
+    let repos = db::repo::get_all_in_context(&conn)?;
+    for repo in repos {
+        db::unit::purge(&conn, &repo)?;
+        cmd::repo::load_units_from_repo(&conn, &repo)?;
+    }
+    Ok(())
+}
