03bf5c95290d6c1defd1cd9b51e1542dca7ce108
Shon Feder <shon.feder@gmail.com>
2021-03-20 12:03:19 -0400

    Clean up and refine anchorification tests

--- src/linkify.rs
+++ src/linkify.rs
@@ -15,27 +15,35 @@ use {
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("Error parsing html during linkification")]
-    ParsingHtml,
+    #[error("Error parsing html during linkification: {0}")]
+    ParsingHtml(String),
+}
+
+fn is_anchor_element(node: &kuchiki::Node) -> bool {
+    match node.as_element() {
+        None => false,
+        Some(el) => {
+            let attrs = el.attributes.borrow();
+            attrs.contains("id")
+        }
+    }
 }
 
 fn as_naked_unit_tag(node: &kuchiki::Node) -> Option<String> {
     match node.first_child() {
         None => None,
         Some(child) => {
-            if let Some(el) = node.as_element() {
-                if let local_name!("a") = el.name.local {
-                    // Bail if the node is already anchored
-                    return None;
-                };
-            };
-            match child.as_text() {
-                None => as_naked_unit_tag(&child),
-                Some(t) => {
-                    let text = t.borrow();
-                    parser::logical_unit_definiendum(&text)
-                        .ok()
-                        .map(|tag| tag.trim_matches('|').into())
+            if is_anchor_element(&child) {
+                None
+            } else {
+                match child.as_text() {
+                    None => as_naked_unit_tag(&child),
+                    Some(t) => {
+                        let text = t.borrow();
+                        parser::logical_unit_definiendum(&text)
+                            .ok()
+                            .map(|tag| tag.trim_matches('|').into())
+                    }
                 }
             }
         }
@@ -53,20 +61,33 @@ fn as_naked_unit_tag(node: &kuchiki::Node) -> Option<String> {
 // TODO Add conn so we can lookup logical unit references
 pub fn linkify_spec_html<R: Read, W: Write + Seek>(reader: &mut R, writer: &mut W) -> Result<()> {
     let doc = kuchiki::parse_html().from_utf8().read_from(reader)?;
-    let terms = doc.select("dt").map_err(|()| Error::ParsingHtml)?;
+    let terms = doc
+        .select("dt")
+        .map_err(|()| Error::ParsingHtml("could not select dt elements".into()))?;
     for term in terms {
         if let Some(tag) = as_naked_unit_tag(term.as_node()) {
+            // TODO Extract to self-documenting helper function
+            // Create a new span element to anchor the tag
             let anchor = kuchiki::NodeRef::new_element(
-                QualName::new(None, ns!(html), local_name!("a")),
+                QualName::new(None, ns!(html), local_name!("span")),
                 Some((
                     ExpandedName::new("", "id"),
                     Attribute {
                         prefix: None,
+                        // Give it the tag's ID as its id attribute
                         value: tag,
                     },
                 )),
             );
-            term.as_node().append(anchor)
+            // Retreive the child of the dt element
+            let child = term
+                .as_node()
+                .first_child()
+                .ok_or_else(|| Error::ParsingHtml("could not get child of dt element".into()))?;
+            // Move the child into the anchor
+            anchor.append(child);
+            // Put the anchor into the dt element
+            term.as_node().append(anchor);
         }
     }
     let res = doc.serialize(writer)?;
@@ -102,35 +123,75 @@ pub fn linkify_spec_file(path: &path::Path) -> Result<()> {
 mod test {
     use super::*;
 
-    #[test]
-    fn can_anchor_spec_html() {
-        let html = r#"<html><head></head><body>
-<dl>
-  <dt>|FOO::1.BAR::1|</dt>
-  <dd>Bam bip blop.</dd>
-  <dt><a id="FOO::1.BAZ::1">|FOO::1.BAZ::1|</a></dt>
-  <dd>Bam bip blop.</dd>
-</dl>
+    fn wrap_as_doc(html: &str) -> String {
+        let mut wrapped = "<html><head></head><body>".to_owned();
+        wrapped.push_str(&html);
+        wrapped.push_str("</body></html>");
+        wrapped
+    }
 
-</body></html>"#
-            .to_string();
-
-        let expected = r#"<html><head></head><body>
-<dl>
-  <dt>|FOO::1.BAR::1|<a id="FOO::1.BAR::1"></a></dt>
-  <dd>Bam bip blop.</dd>
-  <dt><a id="FOO::1.BAZ::1">|FOO::1.BAZ::1|</a></dt>
-  <dd>Bam bip blop.</dd>
-</dl>
-
-</body></html>"#
-            .to_string();
-
-        assert_eq!(expected, linkify_spec_string(&html).unwrap());
+    fn assert_html_transformation(input: &str, expected_output: &str) {
+        let input = wrap_as_doc(input);
+        let expected = wrap_as_doc(expected_output);
+        let actual = linkify_spec_string(&input.to_string()).unwrap();
+        println!("Expected:\n{}", expected);
+        println!("Actual:\n{}", actual);
+        assert_eq!(actual, expected);
     }
 
     #[test]
-    fn can_linkify_spec_html() {
+    fn can_add_anchors_to_spec() {
+        let html = r#"
+<dl>
+  <dt>|FOO.1::BAR.1|</dt>
+  <dd>Bam bip blop.</dd>
+</dl>
+"#;
+        let expected = r#"
+<dl>
+  <dt><span id="FOO.1::BAR.1">|FOO.1::BAR.1|</span></dt>
+  <dd>Bam bip blop.</dd>
+</dl>
+"#;
+        assert_html_transformation(html, expected)
+    }
+
+    #[test]
+    fn can_add_anchors_to_wrapped_elements() {
+        let html = r#"
+<dl>
+  <dt><em>|FOO.1::BIZ.1|</em></dt>
+  <dd>Bam bip blop.</dd>
+</dl>
+"#;
+        let expected = r#"
+<dl>
+  <dt><span id="FOO.1::BIZ.1"><em>|FOO.1::BIZ.1|</em></span></dt>
+  <dd>Bam bip blop.</dd>
+</dl>
+"#;
+        assert_html_transformation(html, expected)
+    }
+
+    #[test]
+    fn does_not_add_redundant_anchors() {
+        let html = r#"
+<dl>
+  <dt><a id="FOO.1::BAZ.1">|FOO.1::BAZ.1|</a></dt>
+  <dd>Bam bip blop.</dd>
+  <dt><span id="FOO.1::BAZ.1">|FOO.1::BAZ.1|</span></dt>
+  <dd>Bam bip blop.</dd>
+  <dt><em id="FOO.1::BOZ.1">|FOO.1::BOZ.1|</em></dt>
+  <dd>Bam bip blop.</dd>
+</dl>
+"#;
+        // Since all the <dt>'s are already anchored, the
+        // transformation should be a no-op.
+        assert_html_transformation(html, html)
+    }
+
+    #[test]
+    fn scratch() {
         let html = r#"<html><head></head><body>
 <dl>
   <dt>|FOO::1.BAR::1|</dt>
