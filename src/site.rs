7e00f6fb6a39559d12b8bac02354acfb8b358e73
Shon Feder <shon.feder@gmail.com>
2021-03-30 20:18:03 -0400

    Use HTML builder in unit tree generation

--- src/site.rs
+++ src/site.rs
@@ -1,11 +1,10 @@
 use {
-    crate::{graph::UnitGraph, logical_unit::LogicalUnit},
-    anyhow::Result,
+    crate::graph::UnitGraph,
     petgraph::{graph::NodeIndex, Direction},
-    std::{fmt, io::Write},
+    std::fmt,
 };
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 enum Html {
     Tag(String, Vec<(String, String)>, Vec<Html>),
     Text(String),
@@ -27,12 +26,8 @@ fn attrs_to_string(attrs: &[(String, String)]) -> String {
 }
 
 macro_rules! tag {
-    ($tag:literal, $attrs:expr, $($child:expr);*) => {
-        { let mut children = Vec::new();
-          $(
-              children.push($child);
-          )*
-          Html::Tag($tag.to_string(), $attrs, children) }
+    ($tag:literal, $attrs:expr, $children:expr) => {
+        Html::Tag($tag.to_string(), $attrs, $children)
     };
 }
 
@@ -79,97 +74,108 @@ impl<'a> fmt::Display for Html {
     }
 }
 
-fn dl_tree<W: Write>(parent_idx: NodeIndex<u32>, graph: &UnitGraph, writer: &mut W) -> Result<()> {
-    let parent = graph.node_weight(parent_idx).unwrap();
-    let mut children = graph
-        .neighbors_directed(parent_idx, Direction::Outgoing)
-        .peekable();
-    write!(
-        writer,
-        r#"<dt id="{0}"><strong>{0}</strong></dt>\n"#,
-        parent.id
-    )?;
-    writeln!(writer, r#"<dd><p class="content">{}</p>"#, parent.content)?;
-    if let Some(_) = children.peek() {
-        write!(
-            writer,
-            r#"<details class="implementations"><summary>Implemented by...</summary>\n<dl>"#
-        )?;
-        for child in children {
-            dl_tree(child, graph, writer)?;
-        }
-        writeln!(writer, "</dl>\n</details>")?;
+impl From<&UnitGraph<'_>> for Html {
+    fn from(graph: &UnitGraph) -> Html {
+        let unit_tree: Vec<Html> = graph
+            .externals(Direction::Incoming)
+            .map(|i| unit_tree_html(i, graph))
+            .collect();
+
+        tag!(
+            "html",
+            vec![],
+            vec![
+                tag!(
+                    "head",
+                    vec![],
+                    vec![tag!("title", vec![], vec![txt!("Context")])]
+                ),
+                tag!("body", vec![], vec![tag!("dl", vec![], unit_tree)])
+            ]
+        )
     }
-    writeln!(writer, "</dd>")?;
-    Ok(())
 }
 
-pub fn unit_tree<W: Write>(graph: &UnitGraph, writer: &mut W) -> Result<()> {
-    let source_idxs = graph.externals(Direction::Incoming);
-    writer.write(
-        r#"
-<html>
-<head>
-<title>Context</title>
-</head>
-<body>
-<dl>
-"#
-        .as_bytes(),
-    )?;
-    for i in source_idxs {
-        dl_tree(i, graph, writer)?
-    }
-    writer.write("</dl>\n</body>\n</html>\n".as_bytes())?;
-    Ok(())
+fn unit_tree_html(parent_idx: NodeIndex<u32>, graph: &UnitGraph) -> Html {
+    let parent = graph.node_weight(parent_idx).unwrap();
+    let mut implementors: Vec<Html> = graph
+        .neighbors_directed(parent_idx, Direction::Outgoing)
+        .map(|child| unit_tree_html(child, graph))
+        .collect();
+    let content = tag!(
+        "dd",
+        vec![],
+        vec![tag!(
+            "p",
+            vec![attr!("class", "content")],
+            vec![txt!(parent.content)]
+        )]
+    );
+
+    let mut children = vec![content];
+    children.append(&mut implementors);
+    tag!("dt", vec![attr!("id", parent.id)], children)
 }
 
 #[cfg(test)]
 mod test {
-    use {super::*, crate::graph, kuchiki, kuchiki::traits::TendrilSink};
+    use {super::*, crate::graph};
 
     #[test]
-    fn can_generate_dl_tree() {
-        let expected = r##"<html><head>
-<title>Context</title>
-</head>
-<body>
-<dl>
-<dt id="FOO.1"><strong>FOO.1</strong></dt>\n<dd><p class="content">Foo content</p>
-<details class="implementations"><summary>Implemented by...</summary>\n<dl><dt id="FOO.1::BING.1"><strong>FOO.1::BING.1</strong></dt>\n<dd><p class="content">Bing content</p>
-</dd>
-<dt id="FOO.1::BAR.1"><strong>FOO.1::BAR.1</strong></dt>\n<dd><p class="content">Bar content</p>
-<details class="implementations"><summary>Implemented by...</summary>\n<dl><dt id="FOO.1::BAR.1::BAZ.1"><strong>FOO.1::BAR.1::BAZ.1</strong></dt>\n<dd><p class="content">Baz content</p>
-</dd>
-</dl>
-</details>
-</dd>
-</dl>
-</details>
-</dd>
-<dt id="FIZ.1"><strong>FIZ.1</strong></dt>\n<dd><p class="content">Fiz content</p>
-</dd>
-</dl>
-
-
-</body></html>"##.to_string();
-        let mut writer = Vec::new();
-        let units = graph::test::test_units();
-        let graph = graph::of_units(&units);
-        unit_tree(&graph, &mut writer).unwrap();
-
-        let mut formatted = Vec::new();
-        kuchiki::parse_html()
-            .from_utf8()
-            .read_from(&mut writer.as_slice())
-            .unwrap()
-            .serialize(&mut formatted)
-            .unwrap();
-        let actual = String::from_utf8_lossy(&formatted);
+    fn html_from_unit_graph() {
+        let actual = Html::from(&graph::of_units(&graph::test::test_units()));
+        let expected = r#"<html >
+  <head >
+    <title >
+        Context
+    </title>
+  </head>
+  <body >
+    <dl >
+      <dt id="FOO.1">
+        <dd >
+          <p class="content">
+              Foo content
+          </p>
+        </dd>
+        <dt id="FOO.1::BING.1">
+          <dd >
+            <p class="content">
+                Bing content
+            </p>
+          </dd>
+        </dt>
+        <dt id="FOO.1::BAR.1">
+          <dd >
+            <p class="content">
+                Bar content
+            </p>
+          </dd>
+          <dt id="FOO.1::BAR.1::BAZ.1">
+            <dd >
+              <p class="content">
+                  Baz content
+              </p>
+            </dd>
+          </dt>
+        </dt>
+      </dt>
+      <dt id="FIZ.1">
+        <dd >
+          <p class="content">
+              Fiz content
+          </p>
+        </dd>
+      </dt>
+    </dl>
+  </body>
+</html>
+"#
+        .to_string();
 
         println!("Expected:\n{}", expected);
         println!("Actual:\n{}", actual);
-        assert_eq!(expected, actual)
+        assert_eq!(expected, actual.to_string())
     }
 
     #[test]
@@ -177,12 +183,18 @@ mod test {
         let html = tag!(
             "html",
             vec![],
-            tag!("head", vec![], tag!("title", vec![], txt!("foo")));
-            tag!(
-                "body",
-                vec![attr!("class", "class1 class2")],
-                tag!("p", vec![], txt!("Some content"))
-            )
+            vec![
+                tag!(
+                    "head",
+                    vec![],
+                    vec![tag!("title", vec![], vec![txt!("foo")])]
+                ),
+                tag!(
+                    "body",
+                    vec![attr!("class", "class1 class2")],
+                    vec![tag!("p", vec![], vec![txt!("Some content")])]
+                )
+            ]
         );
         let actual = html.to_string();
         let expected = r#"<html >
