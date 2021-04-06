38a8300186dd57f9902a323a9d285407d724e77c
Shon Feder <shon.feder@gmail.com>
2021-04-06 19:45:57 -0400

    Add build binaries to path in expect test harness

--- tests/expect_test.rs
+++ tests/expect_test.rs
@@ -12,11 +12,22 @@ fn mdx_tests() {
     let test_artifacts_dir = Path::new("../target/test-artifacts");
     fs::create_dir_all(test_artifacts_dir).unwrap();
 
+    // Add the build executable to the path
+    let mut path = Path::new("../target/debug")
+        .canonicalize()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string();
+    path.push(':');
+    path.push_str(&env::var("PATH").unwrap());
+
     for expect_file in glob("*.md").expect("Failed to read glob pattern") {
         let expected = expect_file.unwrap();
         let corrected = test_artifacts_dir.join(expected.with_extension("md.corrected"));
 
         Command::new("opam")
+            // Put the build executable on the
             .arg("exec")
             .arg("--")
             .arg("ocaml-mdx")
@@ -24,6 +35,7 @@ fn mdx_tests() {
             .arg(expected.to_owned())
             .arg("--output")
             .arg(corrected.to_owned())
+            .env("PATH", &path)
             .assert()
             .success();
 
